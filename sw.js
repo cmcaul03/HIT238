bb8e9d3a88d1d5e39d9e7103107a7e21601f1aeb
Cameron <cameron.mcauliffe7@gmail.com>
2021-10-03 15:23:17 +1100

    Added service worker to allow app to work offline.

---
+++ sw.js
@@ -0,0 +1,47 @@
+var CACHE_TITLE = 'warzone-stats-cache';
+var CACHE_VERSION = 'v1';
+var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION;
+var urlsToCache = [
+  'css/style.css',
+  'js/main.js',
+  '/index.html',
+  'images/ws_logo.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        console.log('cached response' + response)
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME && cacheName.indexOf(CACHE_TITLE) === 0) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
