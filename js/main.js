52ddb5e38145a738393722d8f3a4c118360cfd29
Cameron <cameron.mcauliffe7@gmail.com>
2021-10-10 08:52:17 +1100

    Implemented Weekly Stats

--- js/main.js
+++ js/main.js
@@ -188,6 +188,52 @@ searchButton.addEventListener('click', function(evt) {
 
           statsContent.appendChild(lifetimeStatsSection);
 
+
+          var weeklyStatsSection = document.createElement("Section")
+          weeklyStatsSection.setAttribute("class", "weeklyStats");
+          var h3 = document.createElement("h3")
+          h3.appendChild(document.createTextNode("Weekly Stats"));
+
+
+          var weeklyKDValue = (JSON.stringify(stats.data.segments[1].stats.weeklyKdRatio.value))
+          var weeklyKDValue = parseFloat(weeklyKDValue).toFixed(2)
+
+          var weeklyDeathsValue = parseFloat(((JSON.stringify(stats.data.segments[1].stats.weeklyKills.value)))/weeklyKDValue)
+          var weeklyDeathsValue = Math.round(weeklyDeathsValue)
+
+          if (weeklyDeathsValue == '0') {
+            var weeklyDeathsOneValue = '1'
+          } else {
+            var weeklyDeathsOneValue = weeklyDeathsValue
+          }
+
+
+
+          var weeklyDeaths = document.createElement("div")
+          weeklyDeathsHeaderU = weeklyDeaths.appendChild(document.createElement("U"));
+          weeklyDeathsHeaderU.appendChild(document.createTextNode("Weekly Deaths:"));
+          weeklyDeathsDiv = weeklyDeaths.appendChild(document.createElement("p"));
+          weeklyDeathsDiv.appendChild(document.createTextNode(weeklyDeathsOneValue));
+
+          var weeklyKills = document.createElement("div")
+          weeklyKillsHeaderU = weeklyKills.appendChild(document.createElement("U"));
+          weeklyKillsHeaderU.appendChild(document.createTextNode("Weekly Kills:"));
+          weeklyKillsDiv = weeklyKills.appendChild(document.createElement("p"));
+          weeklyKillsDiv.appendChild(document.createTextNode((JSON.stringify(stats.data.segments[1].stats.weeklyKills.value))));
+
+          var weeklyKD = document.createElement("div")
+          weeklyKDHeaderU = weeklyKD.appendChild(document.createElement("U"));
+          weeklyKDHeaderU.appendChild(document.createTextNode("Weekly KD:"));
+          weeklyKDDiv = weeklyKD.appendChild(document.createElement("p"));
+          weeklyKDDiv.appendChild(document.createTextNode(weeklyKDValue));
+
+          weeklyStatsSection.appendChild(h3)
+          weeklyStatsSection.appendChild(weeklyKills)
+          weeklyStatsSection.appendChild(weeklyDeaths)
+          weeklyStatsSection.appendChild(weeklyKD)
+
+          statsContent.appendChild(weeklyStatsSection);
+
           hideLoadScreen()
           statsContent.style.display = "block";
         }).catch(errorFunction)
